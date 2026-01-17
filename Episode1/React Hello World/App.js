280dac66ffe7a63c6aeeaade80a0e25db3530bbb
ApoorvSharma14 <apoorv03sharma@gmail.com>
2026-01-17 16:45:58 +0530

    nested html elements using react create element

--- Episode1/React Hello World/App.js
+++ Episode1/React Hello World/App.js
@@ -3,4 +3,25 @@ const heading = React.createElement("h1", { class: "heading" }, "Hello World"); 
 // select the root from the html
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // insert the heading tag inside that root
-root.render(heading);
+
+//creating nested elements in react
+/**
+ * <div class = "parent">
+ *  <div class = "child">
+ *      <h1></h1>
+ *      <h1></h1>
+ *  </div>
+ * </div>
+ */
+
+//Creating the above nested element using react and inserting it inside the root container
+const parent = React.createElement(
+  "div",
+  { class: "parent" },
+  React.createElement("div", { class: "child" }, [
+    React.createElement("h1", {}, "Hello"),
+    React.createElement("h1", {}, "World"),
+  ]),
+);
+
+root.render(parent);
